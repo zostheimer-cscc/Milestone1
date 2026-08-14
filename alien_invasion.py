a61572ef2d48a845f6d1084adea3cea62c29fa59
zostheimer-cscc <zostheimer@student.cscc.edu>
2026-08-14 18:48:04 -0400

    commit 3
    fixed the four startup crashes in the main file of alieninvasion.py

---
+++ alien_invasion.py
@@ -0,0 +1,98 @@
+"""
+Program Name: Alien Invasion Main
+
+Author: Zachary Ostheimer
+
+Purpose: This program runs the main game loop for Alien Invasion
+
+Starter Code: Based on the in class Alien Invasion tutorial
+
+Date: 08/12/2026
+"""
+
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from arsenal import Arsenal
+from alien_fleet import AlienFleet
+
+
+class AlienInvasion:
+    #manages the game setup, the main loop, and all events
+
+    def __init__(self) -> None:
+        #start pygame and build the screen, ship, and sound
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w, self.settings.screen_h)
+        )
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg,
+            (self.settings.screen_w, self.settings.screen_h)
+        )
+
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        pygame.mixer.init()
+        self.laser_sound = pygame.mixer.Sound(self.settings.laser_sound)
+        self.laser_sound.set_volume(0.7)
+
+        self.ship = Ship(self, Arsenal(self))
+        self.alien_fleet = AlienFleet(self)
+
+    def run_game(self) -> None:
+        # Game loop
+        while self.running:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(self.settings.FPS)
+
+    def _update_screen(self) -> None:
+        self.screen.blit(self.bg, (0,0))
+        self.ship.draw()
+        self.alien_fleet.draw()
+        pygame.display.flip()
+
+    def _check_events(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keyup_events(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _check_keydown_events(self, event) -> None:
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            if self.ship.fire():
+                self.laser_sound.play()
+                self.laser_sound.fadeout(250)
+
+        elif event.key == pygame.K_q:
+            self.running = False
+            pygame.quit()
+            sys.exit()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
