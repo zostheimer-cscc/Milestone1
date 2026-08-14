9a57d911d88f912ed8c596cd9c3b9bd2c9eb359a
zostheimer-cscc <zostheimer@student.cscc.edu>
2026-08-14 18:51:55 -0400

    commit  5
    redid controls to up/down on W/S and arrows, and removed the last fleet references from the main file

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,7 +15,6 @@ import pygame
 from settings import Settings
 from ship import Ship
 from arsenal import Arsenal
-from alien_fleet import AlienFleet
 
 
 class AlienInvasion:
@@ -44,7 +43,6 @@ class AlienInvasion:
         self.laser_sound.set_volume(0.7)
 
         self.ship = Ship(self, Arsenal(self))
-        self.alien_fleet = AlienFleet(self)
 
     def run_game(self) -> None:
         # Game loop
@@ -57,7 +55,6 @@ class AlienInvasion:
     def _update_screen(self) -> None:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
-        self.alien_fleet.draw()
         pygame.display.flip()
 
     def _check_events(self) -> None:
@@ -72,16 +69,16 @@ class AlienInvasion:
                 self._check_keyup_events(event)
 
     def _check_keyup_events(self, event) -> None:
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
+        if event.key == pygame.K_UP or event.key == pygame.K_w:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+            self.ship.moving_down = False
 
     def _check_keydown_events(self, event) -> None:
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
+        if event.key == pygame.K_UP or event.key == pygame.K_w:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+            self.ship.moving_down = True
         elif event.key == pygame.K_SPACE:
             if self.ship.fire():
                 self.laser_sound.play()
